7f227dd4ce08dc3475323bc5499afaccf4fbcd30
Santy <santiago.valencia4@udea.edu.co>
2022-03-09 14:23:07 -0500

    Feature-HeaderReact

--- frontend/reactapp/src/Components/Header.jsx
+++ frontend/reactapp/src/Components/Header.jsx
@@ -1,9 +1,17 @@
-
+import './header.css';
 
 function head(){
     return(
-      <header>
-          hola
+      <header className ="Header_Container">
+          <ul className ="Header_nav">
+            <a href="http://" className ="Header_nav--item">Home</a>
+            <a href="http://" className ="Header_nav--item">Comprar</a>
+            <a href="http://" className ="Header_nav--item">Vender</a>
+            <a href="http://" className ="Header_nav--item">Calendario</a>
+            <a href="http://" className ="Header_nav--item">Carrito</a>
+            <a href="http://" className ="Header_nav--item perfil_text">Mi perfil</a>
+            
+          </ul>
       </header>
     )
 }
