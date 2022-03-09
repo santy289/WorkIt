c8d7e637ae13dc6b362ce7b0d87ae5812dc0f023
Santy <santiago.valencia4@udea.edu.co>
2022-03-09 14:41:26 -0500

    Feature-layouthome

---
+++ frontend/reactapp/src/Components/Layouthome.jsx
@@ -0,0 +1,11 @@
+import './layouthome.css'
+
+function layouthome(){
+    return(
+        <section className="body-layout">
+            <div className="body-layout--item">1</div>
+            <div className="body-layout--item">2</div>
+        </section>
+    )
+}
+export default layouthome;
