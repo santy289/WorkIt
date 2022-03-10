f440ffc97a18a5c7db81d2938a5af90c63e3be90
Santy <santiago.valencia4@udea.edu.co>
2022-03-09 19:27:26 -0500

    feature-mediaquires

--- frontend/reactapp/src/Components/Header.jsx
+++ frontend/reactapp/src/Components/Header.jsx
@@ -3,14 +3,14 @@ import './header.css';
 function header(){
     return(
       <header className ="Header_Container">
-          <img className ="Header_img" src="../assets/workit.svg" alt="" />
           <ul className ="Header_nav">
-            <a href="http://" className ="Header_nav--item">Home</a>
-            <a href="http://" className ="Header_nav--item">Comprar</a>
-            <a href="http://" className ="Header_nav--item">Vender</a>
-            <a href="http://" className ="Header_nav--item">Calendario</a>
-            <a href="http://" className ="Header_nav--item">Carrito</a>
-            <a href="http://" className ="Header_nav--item perfil_text">Mi perfil</a>
+            <img className ="Header_img" src="https://raw.githubusercontent.com/santy289/WorkIt/develop/frontend/reactapp/src/assets/smalllogowithe.jpg" alt="" />
+            <a href="#" className ="Header_nav--item">Home</a>
+            <a href="#" className ="Header_nav--item">Comprar</a>
+            <a href="#" className ="Header_nav--item">Vender</a>
+            <a href="#" className ="Header_nav--item">Calendario</a>
+            <a href="#" className ="Header_nav--item">Carrito</a>
+            <a href="#" className ="Header_nav--item perfil_text">Mi perfil</a>
           </ul>
       </header>
     )
