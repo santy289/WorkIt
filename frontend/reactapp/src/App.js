fa6aaccdc9b8614bf932907ac6397ab99fd37a66
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-10 02:17:51 -0500

    feat: Add a  json in assets to mock info in categories, add a component called CategoriesCard to implement in app with the respective css, changes in app to receive info in card, changes in app.css to organize the cards an add a title of cards

--- frontend/reactapp/src/App.js
+++ frontend/reactapp/src/App.js
@@ -1,7 +1,8 @@
 import Head from './components/Header.jsx';
 import Layouthome from './components/Layouthome.jsx';
 import HomeBrand from './components/Homebrand.jsx'
-
+import Categoriescard from './components/Categoriescard.jsx'
+import servicesList from './assets/categoriesinfo.json'
 import './App.css';
 
 function App() {
@@ -9,6 +10,14 @@ function App() {
     <div className="App">
      <Head/>
      <HomeBrand/>
+
+     <h1 className='containerTitle'>Servicios destacados</h1>
+     <div className='containerCard'>
+        
+       {servicesList.map((service, idx) => 
+       <Categoriescard key={idx} service = {service}/>)}
+      </div>
+
      <Layouthome/>     
     </div>
   );
