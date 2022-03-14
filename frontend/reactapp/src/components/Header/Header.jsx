ff2b5fb0c5794f54b01f4b6120d69e3c0bcf85ad
Santy <santiago.valencia4@udea.edu.co>
2022-03-14 13:42:38 -0500

    Feature-HamburguerMenu

--- frontend/reactapp/src/components/Header/Header.jsx
+++ frontend/reactapp/src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import './header.css';
+import Sidebar from '../HamburguerMenu/Sidebar';
 
 function header(){
     return(
       <header className ="Header_Container">
+        <Sidebar/>
           <ul className ="Header_nav">
             <img className ="Header_img" src="https://raw.githubusercontent.com/santy289/WorkIt/develop/frontend/reactapp/src/assets/smalllogowithe.jpg" alt="" />
-            <a href="#" className ="Header_nav--item">Home</a>
-            <a href="#" className ="Header_nav--item">Comprar</a>
-            <a href="#" className ="Header_nav--item">Vender</a>
-            <a href="#" className ="Header_nav--item">Calendario</a>
-            <a href="#" className ="Header_nav--item">Carrito</a>
-            <a href="#" className ="Header_nav--item perfil_text">Mi perfil</a>
+            <a to="#" className ="Header_nav--item">Home</a>
+            <a to="#" className ="Header_nav--item">Comprar</a>
+            <a to="#" className ="Header_nav--item">Vender</a>
+            <a to="#" className ="Header_nav--item">Calendario</a>
+            <a to="#" className ="Header_nav--item">Carrito</a>
+            <a to="#" className ="Header_nav--item perfil_text">Mi perfil</a>
           </ul>
       </header>
     )
