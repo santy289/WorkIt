6f021c14a5b35f0b6d04599a6dd084661219fd8e
Santy <santiago.valencia4@udea.edu.co>
2022-03-18 13:50:12 -0500

    Fix-Routes/Header

--- frontend/reactapp/src/components/Header/Header.jsx
+++ frontend/reactapp/src/components/Header/Header.jsx
@@ -11,23 +11,13 @@ function Header() {
     <header className="Header_Container">
       <Sidebar />
       <ul className="Header_nav">
-<<<<<<< HEAD
-        <img className="Header_img" src="https://raw.githubusercontent.com/santy289/WorkIt/develop/frontend/reactapp/src/assets/smalllogowithe.jpg" alt="" />
-        <a to="#" className="Header_nav--item">Home</a>
-        <a to="#" className="Header_nav--item">Comprar</a>
-        <a to="#" className="Header_nav--item">Vender</a>
-        <a to="#" className="Header_nav--item">Calendario</a>
-        <a to="#" className="Header_nav--item">Carrito</a>
-        <a to="#" className="Header_nav--item perfil_text">Iniciar Sesión</a>
-=======
         <img className="Header_img" src={smallLogo} alt="" />
         <NavLink to={ROUTE_HOME} className="Header_nav--item">Home</NavLink>
         <NavLink to={ROUTE_PURCHASER} className="Header_nav--item">Comprar</NavLink>
         <NavLink to={ROUTE_SELLER} className="Header_nav--item">Vender</NavLink>
         <NavLink to="#" className="Header_nav--item">Calendario</NavLink>
         <NavLink to="#" className="Header_nav--item">Carrito</NavLink>
-        <NavLink to={ROUTE_USERPROFILE} className="Header_nav--item perfil_text">Mi perfil</NavLink>
->>>>>>> 070ca9322f486a295b268e666219ae59de5a84fd
+        <NavLink to={ROUTE_USERPROFILE} className="Header_nav--item perfil_text">iniciar Sesión</NavLink>
       </ul>
     </header>
   );
--- frontend/reactapp/src/components/Mainrouter/Mainrouter.jsx
+++ frontend/reactapp/src/components/Mainrouter/Mainrouter.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Header from '../Header/Header';
-import { ROUTE_HOME, ROUTE_LANDING, ROUTE_PURCHASER, ROUTE_SELLER, ROUTE_USERPROFILE } from '../Constants/Routes';
+import {
+  ROUTE_HOME, ROUTE_LANDING, ROUTE_PURCHASER, ROUTE_SELLER, ROUTE_USERPROFILE,
+} from '../Constants/Routes';
 import LandingPage from '../../pages/LandingPage/LandingPage';
 import Purchaser from '../../pages/Purchaser/Purchaser';
 import Seller from '../../pages/Seller/Seller';
