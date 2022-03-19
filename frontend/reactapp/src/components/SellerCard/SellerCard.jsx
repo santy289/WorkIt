a9b5fb7356ffd1a2ba2729239ec8325328a5a95e
Santy <santiago.valencia4@udea.edu.co>
2022-03-18 23:20:30 -0500

    install

---
+++ frontend/reactapp/src/components/SellerCard/SellerCard.jsx
@@ -0,0 +1,10 @@
+import database from '/MakeItReal/WorkIt/frontend/reactapp/db.json';
+
+function Sellercard() {
+  return (
+    <div className='cards_container'>
+
+    </div>
+  );
+}
+export default Sellercard;
