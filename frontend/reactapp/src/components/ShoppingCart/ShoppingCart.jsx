e197fbaf0b15fd9f72851a8cf138133cd1cd6251
Daniel <danielatts97@hotmail.com>
2022-03-22 06:18:40 -0500

    add shoppingcart component

---
+++ frontend/reactapp/src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,30 @@
+import Button from '../Button/Button';
+import cart from '../../assets/shopping-cart.svg';
+import './ShoppingCart.styles.scss';
+
+function ShoppingCart() {
+  return (
+    <div className="shoppingcart__card">
+      <div className="shoppingcart__content">
+        <div className="shoppingcart__title">
+          <h2>Carrito de compras</h2>
+          <span><img src={cart} alt="cart" className="icon" /></span>
+        </div>
+        <div className="shoppingcart__items">
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+        </div>
+        <div className="shoppingcart__total">
+          <h4>Total: </h4>
+          <h3>$$$</h3>
+        </div>
+        <div className="shoppingcart__purchase">
+          <Button text="Comprar" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShoppingCart;
