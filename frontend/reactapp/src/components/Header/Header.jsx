df25a813b1daf39b6ad0acd0bc30e1b6498c9344
Daniel <danielatts97@hotmail.com>
2022-03-22 07:12:00 -0500

    update searchbar stylesheet to BEM

--- frontend/reactapp/src/components/Header/Header.jsx
+++ frontend/reactapp/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
         <NavLink to={ROUTE_PURCHASER} className="Header_nav--item">Comprar</NavLink>
         <NavLink to={ROUTE_SELLER} className="Header_nav--item">Vender</NavLink>
         <NavLink to="#" className="Header_nav--item">Carrito</NavLink>
-        <NavLink to={ROUTE_LOGIN} className="Header_nav--item perfil_text">iniciar Sesión</NavLink>
+        <NavLink to={ROUTE_LOGIN} className="Header_nav--item perfil_text">Iniciar Sesión</NavLink>
         <NavLink to={ROUTE_SIGNUP} className="Header_nav--item">Registrarse</NavLink>
         <NavLink to={ROUTE_USERPROFILE} className="Header_nav--item">Perfil</NavLink>
 
