a54d6293579a133894937300e3b37f9e9aeca999
Julian Salamanca <jsalamancah@unal.edu.co>
2022-04-12 21:10:21 -0500

    filter component changed

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Filter.scss';
+
+function Filter() {
+  return (
+    <div className="container">
+      <p>
+        Buscar
+      </p>
+      <div className="form__group field">
+        <input type="input" className="form__field" placeholder="Buscar" name="name" id="name" />
+      </div>
+      <p>
+        por :
+      </p>
+      <form>
+        <label htmlFor="producto">
+          <input id="producto" type="checkbox" name="producto" />
+          <span>Producto/servicio</span>
+        </label>
+        <label htmlFor="vendedor">
+          <input id="vendedor" type="checkbox" name="vendedor" />
+          <span>Vendedor</span>
+        </label>
+        <label htmlFor="tag">
+          <input id="tag" type="checkbox" name="tag" />
+          <span>Tag</span>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Filter;
