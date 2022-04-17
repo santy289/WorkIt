005c80d8845ced2ecdc94e4cade73b08e0616359
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-04-16 19:37:58 -0500

    feat: added validations for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,38 +1,64 @@
+/* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* este comment se arrergla asignando url a los hash en las etiquetas a */
-import { useState } from 'react';
+import React from 'react';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
 import Button from '../Button/Button';
 import './Login.styles.scss';
 
 function Login() {
-  const [form, setForm] = useState({
-    username: '',
-    password: '',
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(form);
-  };
-
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h2 className="login__title">INICIA SESIÓN</h2>
-      <label className="login__text" htmlFor="username">Usuario</label>
-      <input type="text" id="username" name="username" onChange={handleChange} />
-      <label className="login__text" htmlFor="password">Contraseña</label>
-      <input type="password" id="password" name="password" onChange={handleChange} />
-      <a className="login__link" href="#">¿Olvidaste tu contraseña?</a>
-      <Button className="login__submit" type="submit" text="INICIAR SESIÓN" />
-    </form>
+    <Formik
+      initialValues={{
+        username: '',
+        password: '',
+      }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.username) {
+          errors.username = 'Campo requerido';
+        }
+        if (!values.password) {
+          errors.password = 'Campo requerido';
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ errors }) => (
+        <Form className="login">
+          <h2 className="login__title">INICIA SESIÓN</h2>
+          <label className="login__text" htmlFor="username">Usuario</label>
+          <ErrorMessage
+            name="username"
+            component={() => (
+              <div className="login__error">
+                {errors.username}
+              </div>
+            )}
+          />
+          <Field type="text" id="username" name="username" />
+          <label className="login__text" htmlFor="password">Contraseña</label>
+          <ErrorMessage
+            name="password"
+            component={() => (
+              <div className="login__error">
+                {errors.password}
+              </div>
+            )}
+          />
+          <Field type="password" id="password" name="password" />
+          <a className="login__link" href="#">¿Olvidaste tu contraseña?</a>
+          <Button className="login__submit" type="submit" text="INICIAR SESIÓN" />
+        </Form>
+      )}
+
+    </Formik>
   );
 }
 
