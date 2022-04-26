ff9706e3c9480ed4d457484e3952a4383ccfa2e3
Santy <santiago.valencia4@udea.edu.co>
2022-04-25 21:29:26 -0500

    Feature: Checkout Page/ Checkout Card

--- src/components/CheckoutCard/index.jsx
+++ src/components/CheckoutCard/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-function CheckoutCard() {
+function CheckoutCard(props) {
+  // eslint-disable-next-line react/prop-types
+  const { paymentId, status, paymentType } = props;
   return (
-    <div>CheckoutCard</div>
+    <div>
+      <h1>Id de pago</h1>
+      <p>{paymentId}</p>
+      <h1>Estado de pago</h1>
+      <p>{status}</p>
+      <h1>Tipo de pago</h1>
+      <p>{paymentType}</p>
+    </div>
   );
 }
 
--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import queryString from 'query-string';
+import Header from '../../components/Header/Header';
 import CheckoutCard from '../../components/CheckoutCard';
 
+// eslint-disable-next-line no-restricted-globals
+const value = queryString.parse(location.search);
+const { status, paymentId, paymentType } = value;
+
 function CheckoutPage() {
+  if (status === 'success') {
+    return (
+      <div>
+        <CheckoutCard
+          paymentId={paymentId}
+          status={status}
+          paymentType={paymentType}
+        />
+      </div>
+    );
+  }
   return (
-    <CheckoutCard />
+    <div>
+      <Header />
+      <h1>Payment Status Fail</h1>
+    </div>
   );
 }
 
