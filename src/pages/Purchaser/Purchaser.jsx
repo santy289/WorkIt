7bb0cece56416446deeed58a6dff00c971e0989d
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-05-04 11:09:03 -0500

    hotfix: changes with solved conflicts

--- src/pages/Purchaser/Purchaser.jsx
+++ src/pages/Purchaser/Purchaser.jsx
@@ -25,12 +25,8 @@ function Purchaser() {
           <Filter />
         </div>
         <div className="purchaser__body">
-<<<<<<< HEAD
           <h1>Servicios Disponibles</h1>
           <div className="purcasher_list">
-=======
-          <div className="purchaser__list">
->>>>>>> 94bb4cf98d90ff39e4223b2410056cbf6403c949
             {
               (services.map((service) => (
                 <CardService key={service._id} eachService={service} />
